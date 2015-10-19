fa13b54420e0a2e2c40da9774addabcc0acfb052
natruja <plelek69@gmail.com>
2015-10-19 23:15:43 +0700

    Commit

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once 'core/init.php';
 
-$user = DB::getInstance()->query("SELECT emp_id FROM all_ro10_emp WHERE emp_id = ?", array('26-1136'));
-if ($user->error()) {
-    	echo "No user";
-} else {
-    	echo "ok";
-}
+		$user = DB::getInstance()->query("SELECT emp_id FROM all_ro10_emp WHERE emp_id = ?", array('26-1136'));
+		if ($user->error()) {
+		    	echo "No user";
+		} else {
+		    	echo "ok";
+		}
 ?>
